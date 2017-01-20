ebc609d5b95fc1cd901fbe2e1a4565077bb7b133
Lilo <lilo@hpe.com>
2017-01-20 20:28:15 +0200

    Added wet logic and timer

--- Assets/Resources/Tiles/Scripts/SandTile.cs
+++ Assets/Resources/Tiles/Scripts/SandTile.cs
@@ -6,16 +6,31 @@ using UnityEngine;
 
 namespace Tiles {
     class SandTile : MonoBehaviour {
+        SpriteRenderer sprite;
+
         public float DryTime = 2f;
+        private Color WetColor = new Color(0.7f, 0.7f, 0.7f);
+        private Color DryColor = new Color(1, 1, 1);
         private bool isWet;
+
+        private void Start() {
+            sprite = GetComponent<SpriteRenderer>();
+            
+        }
+
         private void OnCollisionEnter2D(Collision2D collision) {
             if (collision.collider.tag == "Water") {
                 isWet = true;
+                sprite.color = WetColor;
                 Invoke("DryUp", DryTime);
             }
         }
         private void DryUp() {
             isWet = false;
+            sprite.color = DryColor;
+
         }
+
+
     }
 }
