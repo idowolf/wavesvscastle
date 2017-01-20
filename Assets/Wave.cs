feca26c81bb0e2fe84e1b62c2699d08f3185c258
Ido Wolf <idowolf@gmail.com>
2017-01-20 17:47:11 +0200

    Made simple code for grid

--- Assets/Wave.cs
+++ Assets/Wave.cs
@@ -3,9 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Wave : MonoBehaviour {
-
+    public float heightMargin = 0.35f, widthMargin = 0.35f; // Magic numbers, set in editor for best optimization
 	// Use this for initialization
 	void Start () {
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                GameObject.Instantiate(Resources.Load("GrassPrefab"), new Vector3(-1.433f + j * widthMargin, 0.84f - heightMargin * i), Quaternion.identity);
+            }
+        }
+            
+
 		
 	}
 	
