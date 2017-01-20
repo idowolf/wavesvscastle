40a998f8e004fb8f4bcd43431c074d87bbdfb195
DanPo8 <dandan888@gmail.com>
2017-01-20 20:38:39 +0200

    Fixed Camera and Added Barricade Prefab

--- Assets/Resources/MapManager.cs
+++ Assets/Resources/MapManager.cs
@@ -7,9 +7,10 @@ public class MapManager : MonoBehaviour {
     // Use this for initialization
 
     private const int LANES = 5;
-    private const int WATER_ROWS = 2;
-    private const int SAND_ROWS = 6;
+    private const int WATER_ROWS = 1;
+    private const int SAND_ROWS = 5;
     private const int CASTLE_ROWS = 2;
+    private const int TOOLBAR = 1;
     private const float tilesize = 2.239f;
     public GameObject WaterTile;
     public GameObject SandTile;
