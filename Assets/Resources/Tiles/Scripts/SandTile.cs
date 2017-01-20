63976988124adcc8f220cb0bda11444387e970a9
Lilo <lilo@hpe.com>
2017-01-20 18:52:29 +0200

    Added tiles prefabs. Removed non existing scripts folder meta file.

---
+++ Assets/Resources/Tiles/Scripts/SandTile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Tiles {
+    class SandTile : MonoBehaviour {
+        private void OnCollisionEnter2D(Collision2D collision) {
+            if(collision.collider.tag == "Water") {
+
+            }
+        }
+    }
+}
