8e003f077ec4fa31433f16f2b7db928c9fc3a13b
Ido Wolf <idowolf@gmail.com>
2017-01-20 18:20:44 +0200

    Renames

--- Assets/FakeGrassFactory.cs
+++ Assets/FakeGrassFactory.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FakeGrassFactory : MonoBehaviour {
+    public float heightMargin = 0.35f, widthMargin = 0.35f; // Magic numbers, set in editor for best optimization
+                                                            // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                GameObject.Instantiate(Resources.Load("GrassPrefab"), new Vector3(-1.433f + j * widthMargin, 0.84f - heightMargin * i), Quaternion.identity);
+            }
+        }
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        // Update is called once per frame
+        void Update () {
 		
 	}
 }
