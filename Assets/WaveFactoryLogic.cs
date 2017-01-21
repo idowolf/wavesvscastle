4097cbf7b26ad61d1e350f1226838a3a0fa85bb4
Lilo <lilo@hpe.com>
2017-01-21 11:46:56 +0200

    Fixed: - splashes not destroyed - different wave speed on different devices (added deltaTime)

--- Assets/WaveFactoryLogic.cs
+++ Assets/WaveFactoryLogic.cs
@@ -22,7 +22,6 @@ namespace Castle
         }
         void addWave()
         {
-            Debug.Log("Fucknimrod2000");
             //set spawn point to be random in the range or at the middle of it 
             Vector2 spawnPoint = new Vector2(2.24f * Random.Range(0, 5), -0.5f);
             //= new Vector2(transform.position.x , Random.Range(y1, y2));
